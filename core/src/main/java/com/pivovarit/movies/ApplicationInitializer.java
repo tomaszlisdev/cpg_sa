cbf24b20dcb7b56b87aeda8bf83693b0527353ca
tomasz.lis <S@m8un6$>
2019-03-07 00:24:01 +0100

    property sources dowloaded to temp dir

--- core/src/main/java/com/pivovarit/movies/ApplicationInitializer.java
+++ core/src/main/java/com/pivovarit/movies/ApplicationInitializer.java
@@ -4,6 +4,7 @@ import com.pivovarit.movies.domain.MovieFacade;
 import com.pivovarit.movies.dto.MovieDto;
 import com.pivovarit.movies.dto.MovieTypeDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,9 @@ class ApplicationInitializer implements CommandLineRunner {
 
     private final MovieFacade movieFacade;
 
+    @Value("${java.io.tmpdir}")
+    private String tempDir;
+
     @Override
     public void run(String... args) {
         System.out.println("Started!");
@@ -20,5 +24,6 @@ class ApplicationInitializer implements CommandLineRunner {
         movieFacade.add(new MovieDto(1L, "Spiderman1", new MovieTypeDto("NEW")));
         movieFacade.priceFor(new MovieTypeDto("NEW"))
             .ifPresent(System.out::println);
+        System.out.println(tempDir);
     }
 }

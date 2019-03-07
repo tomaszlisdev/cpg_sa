8e95c8301abf207f19b35dce121c268a3357d449
tomasz.lis <S@m8un6$>
2019-03-07 09:38:23 +0100

    eureka server and movie details added

---
+++ movie-details/src/main/java/com/pivovar/moviedetails/MovieDetailsApplication.java
@@ -0,0 +1,11 @@
+package com.pivovar.moviedetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDetailsApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MovieDetailsApplication.class, args);
+    }
+}
